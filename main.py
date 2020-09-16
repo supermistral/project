13345404756163e86a4c48c20d311ae7c52150ab
supermistral <53310165+supermistral@users.noreply.github.com>
2020-09-16 15:46:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import function as func
 def msg(user_id, Message):
     vk.messages.send(user_id = event.user_id, random_id = get_random_id(), message = Message)    
     
-vk_sess = vk_api.VkApi(token = "b2988ed57b558cca8bb1b8ec5e4b998a45214845e964fca9463dd09e2f79c62e9e6f900a8451e6a5f6709")
+vk_sess = vk_api.VkApi(token = "") #Поместить токен бота
 vk = vk_sess.get_api()
 longpoll = VkLongPoll(vk_sess)
 
